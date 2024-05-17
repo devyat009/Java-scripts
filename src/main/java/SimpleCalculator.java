89bf6f30d41634cc505463fc26808dd0d4892c39
devyat009 <91706455+devyat009@users.noreply.github.com>
2024-05-17 17:05:10 +0000

    Code restructuration and fix in the user input validation
    |PT-BR|
    — Restruturação do código.
    — Correção na validação de input do usuário.
      Exemplo: se o usuário inserir uma letra, ira alertar que um caractere invalido foi inserido.
    |EN|
    — Code restructuring
    — Correction in user input validation.
        Example: if the user enters a letter, it will alert that an invalid character has been entered.

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -11,7 +11,9 @@
 // Last update date:
 
 import java.util.Scanner;
+
 public class SimpleCalculator {
+
     public static void main(String[] args) {
         // |PT-BR| Titulo do programa:
         // |EN| Title of the program:
@@ -22,104 +24,110 @@ public class SimpleCalculator {
         // |PT-BR| Variáveis:
         // |EN| Variables:
         float resultado = 0;
+        int operacao;
         // |PT-BR| Inserir o primeiro valor:
         // |EN| Insert the first value:
         System.out.println("Insira o PRIMEIRO valor: ");
-        while (!scanner.hasNextFloat()) {
-            System.out.println("Por favor insira numeros: ");
-            scanner.next();
-        }
-        float numero_1 = scanner.nextFloat();
+        float numero_1 = inputUsuario(scanner);
         // |PT-BR| Inserir o segundo valor:
         // |EN| Insert the second value:
         System.out.println("Insira o SEGUNDA valor: ");
+        float numero_2 = inputUsuario(scanner);
+        //
+        boolean inputIsValid = false;
+        while (!inputIsValid) {
+            // |PT-BR| Opções para o usuario selecionar a operação:
+            // |EN| Options for the user to select the operation:
+            System.out.println("Selecione a operação: ");
+            System.out.println("1 - Soma");
+            System.out.println("2 - Subtração");
+            System.out.println("3 - Multiplicação");
+            System.out.println("4 - Divisão");
+            System.out.println("5 - Potência");
+            System.out.println("6 - Raiz Quadrada");
+            System.out.println("7 - Raiz Cúbica");
+            System.out.println("8 - Sair");
+            System.out.print("Operação: ");
+            operacao = scanner.nextInt();
+            // |PT-BR| Operações matemáticas com a resposta do programa:
+            // |EN| Mathematical operations with the answer of the program:
+            switch (operacao) {
+                // |PT-BR| Opção 1: Soma
+                // |EN| Option 1: Addition
+                case 1:
+                    resultado = numero_1 + numero_2;
+                    System.out.println("O resultado da soma é: " + resultado);
+                    break;
+                // |PT-BR| Opção 2: Subtração
+                // |EN| Option 2: Subtraction
+                case 2:
+                    resultado = numero_1 - numero_2;
+                    System.out.println("o resultado da subtração é: " + resultado);
+                    break;
+                // |PT-BR| Opção 3: Multiplicação
+                // |EN| Option 3: Multiplication
+                case 3:
+                    resultado = numero_1 * numero_2;
+                    System.out.println("O resultado da multiplicação é: " + resultado);
+                    break;
+                // |PT-BR| Opção 4: Divisão
+                // |EN| Option 4: Division
+                case 4:
+                    if (numero_1 == 0 || numero_2 == 0) {
+                        System.out.println("Não é possível dividir por 0, portanto o resultado é: 0");
+                    } else {
+                        resultado = numero_1 / numero_2;
+                        System.out.println("O resultado da divisão é: " + resultado);
+                    }
+                    break;
+                // |PT-BR| Opção 5: Potência
+                // |EN| Option 5: Exponentiation
+                case 5:
+                    resultado = (float) Math.pow(numero_1, numero_2);
+                    System.out.println("O resultado da potência é: " + resultado);
+                    break;
+                // |PT-BR| Opção 6: Raiz Quadrada
+                // |EN| Option 6: Square Root
+                case 6:
+                    resultado = (float) Math.sqrt(numero_1);
+                    System.out.println("O resultado da raiz quadrada do primeiro numero é: " + resultado);
+                    resultado = (float) Math.sqrt(numero_2);
+                    System.out.println("O resultado da raiz quadrada do segundo numero é: " + resultado);
+                    break;
+                // |PT-BR| Opção 7: Raiz Cúbica
+                // |EN| Option 7: Cubic Root
+                case 7:
+                    resultado = (float) Math.cbrt(numero_1);
+                    System.out.println("O resultado da raiz cúbica do primeiro numero é: " + resultado);
+                    resultado = (float) Math.cbrt(numero_2);
+                    System.out.println("O resultado da raiz cúbica do segundo numero é: " + resultado);
+                    break;
+                // |PT-BR| Opção 8: Sair
+                // |EN| Option 8: Exit
+                case 8:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Invalid day! Please enter a number between 1 and 8.");
+                } // switch end
+
+        } // While END
+
+        // |PT-BR| Loop para verificar se a opção selecionada é válida:
+        // |EN| Loop to check if the selected option is valid:
+
+        // |PT-BR| Fecha o scanner
+        // |EN| Close the scanner
+        scanner.close();
+    } // End of the public main void
+    
+    // |PT-BR| Função para ler o input do usuário
+    // |EN| Function to read user input
+    public static float inputUsuario(Scanner scanner){
         while (!scanner.hasNextFloat()) {
             System.out.println("Por favor insira numeros: ");
             scanner.next();
         }
-        float numero_2 = scanner.nextFloat();
-        
-        // |PT-BR| Opções para o usuario selecionar a operação:
-        // |EN| Options for the user to select the operation:
-        System.out.println("Selecione a operação: ");
-        System.out.println("1 - Soma");
-        System.out.println("2 - Subtração");
-        System.out.println("3 - Multiplicação");
-        System.out.println("4 - Divisão");
-        System.out.println("5 - Potência");
-        System.out.println("6 - Raiz Quadrada");
-        System.out.println("7 - Raiz Cúbica");
-        System.out.println("8 - Sair");
-        System.out.print("Operação: ");
-        // |PT-BR| Loop para verificar se a opção selecionada é válida:
-        // |EN| Loop to check if the selected option is valid:
-        // Algo de errado aqui, não sei o que, incapaz de detectar letras, crash no terminal
-        while (!scanner.hasNextInt()) {
-            System.out.println("Insira uma opção válida: ");
-            scanner.next(); // consume the invalid input
-        }
-        int operacao = scanner.nextInt();
-        // |PT-BR| Operações matemáticas com a resposta do programa:
-        // |EN| Mathematical operations with the answer of the program:
-        switch (operacao) {
-            // |PT-BR| Opção 1: Soma
-            // |EN| Option 1: Addition   
-            case 1:
-                resultado = numero_1 + numero_2;
-                System.out.println("O resultado da soma é: " + resultado);
-                break;
-            // |PT-BR| Opção 2: Subtração
-            // |EN| Option 2: Subtraction
-            case 2:
-                resultado = numero_1 - numero_2;
-                System.out.println("o resultado da subtração é: " + resultado);
-                break;
-            // |PT-BR| Opção 3: Multiplicação
-            // |EN| Option 3: Multiplication
-            case 3:
-                resultado = numero_1 * numero_2;
-                System.out.println("O resultado da multiplicação é: " + resultado);
-                break;
-            // |PT-BR| Opção 4: Divisão
-            // |EN| Option 4: Division
-            case 4:
-                if (numero_1 == 0 || numero_2 == 0) {
-                    System.out.println("Não é possível dividir por 0, portanto o resultado é: 0");                
-                } else {
-                    resultado = numero_1 / numero_2;
-                    System.out.println("O resultado da divisão é: " + resultado);
-                }
-                break;
-            // |PT-BR| Opção 5: Potência
-            // |EN| Option 5: Exponentiation
-            case 5:
-                resultado = (float) Math.pow(numero_1, numero_2);
-                System.out.println("O resultado da potência é: " + resultado);
-                break;
-            // |PT-BR| Opção 6: Raiz Quadrada
-            // |EN| Option 6: Square Root
-            case 6:
-                resultado = (float) Math.sqrt(numero_1);
-                System.out.println("O resultado da raiz quadrada do primeiro numero é: " + resultado);
-                resultado = (float) Math.sqrt(numero_2);
-                System.out.println("O resultado da raiz quadrada do segundo numero é: " + resultado);
-                break;
-            // |PT-BR| Opção 7: Raiz Cúbica
-            // |EN| Option 7: Cubic Root
-            case 7:
-                resultado = (float) Math.cbrt(numero_1);
-                System.out.println("O resultado da raiz cúbica do primeiro numero é: " + resultado);
-                resultado = (float) Math.cbrt(numero_2);
-                System.out.println("O resultado da raiz cúbica do segundo numero é: " + resultado);
-                break;
-            // |PT-BR| Opção 8: Sair
-            // |EN| Option 8: Exit
-            case 8:
-                System.out.println("Saindo...");
-                break;
-        }
-        // |PT-BR| Fecha o scanner
-        // |EN| Close the scanner
-        scanner.close();
-    }
-}
+        return scanner.nextFloat();
+    } 
+}// End of the main class
